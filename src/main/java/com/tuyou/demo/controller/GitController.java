368ed3d2285aa569547dc36d77cb0b0377506f16
chenyk1992 <353200714@qq.com>
2018-10-20 18:33:00 +0800

    idea commit class

---
+++ src/main/java/com/tuyou/demo/controller/GitController.java
@@ -0,0 +1,10 @@
+package com.tuyou.demo.controller;
+
+/**
+ * @className: GitController
+ * @description: 测试GIT
+ * @author: Administrator
+ * @create: 2018-10-20 18:31
+ **/
+public class GitController {
+}
